83b61d2f6f11ed967d0c4a50c45bf6dba080c111
Egor Kondrashov <e.kondr01@gmail.com>
2021-12-12 10:24:27 +0300

    fix: delete trailing slash

--- config/urls.py
+++ config/urls.py
@@ -16,13 +16,13 @@ if bool(settings.DEBUG):
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
     urlpatterns += [
-        path('api/schema/',
+        path('api/schema',
              SpectacularAPIView.as_view(),
              name='schema'),
-        path('api/schema/swagger-ui/',
+        path('api/schema/swagger-ui',
              SpectacularSwaggerView.as_view(url_name='schema'),
              name='swagger-ui'),
-        path('api/schema/redoc/',
+        path('api/schema/redoc',
              SpectacularRedocView.as_view(url_name='schema'),
              name='redoc'),
     ]
